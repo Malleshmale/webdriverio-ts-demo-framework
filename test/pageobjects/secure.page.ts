d27edb88f6b64f1ebcbfae1a321d1a583539f9cd
Malleshmale <162980792+Malleshmale@users.noreply.github.com>
2025-05-04 12:01:07 +0530

    Update secure.page.ts

--- test/pageobjects/secure.page.ts
+++ test/pageobjects/secure.page.ts
@@ -2,9 +2,7 @@ import { ChainablePromiseElement } from 'webdriverio';
 
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class SecurePage extends Page {
     /**
      * define selectors using getter methods

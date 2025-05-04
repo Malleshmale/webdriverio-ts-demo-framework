095e5ebadfc8667cd2f695466cc89651dcd44cac
Malleshmale <162980792+Malleshmale@users.noreply.github.com>
2025-05-04 12:00:39 +0530

    Update login.page.ts

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -2,13 +2,8 @@ import { ChainablePromiseElement } from 'webdriverio';
 
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+    
     public get inputUsername(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
         return $('#username');
     }
@@ -20,23 +15,13 @@ class LoginPage extends Page {
     public get btnSubmit(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
         return $('button[type="submit"]');
     }
-    // public get btnSubmit(): ChainablePromiseElement<Promise<WebdriverIO.Element>> {
-    //     return $('button[type="submit"]');
-    // }
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    
     public async login (username: string, password: string): Promise<void> {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     public open(): Promise<string> {
         return super.open('login');
     }
